f9f7f04713c0f9f920e760dd55f513c497a6d9b3
Christopher Martin <cgmartin@gmail.com>
2013-06-10 22:50:06 -0400

    Remove unnecessary dirs, add details to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject clj-wamp-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-wamp-example "0.1.0"
+  :description "An example http-kit websocket project with clj-wamp"
+  :url "https://github.com/cgmartin/clj-wamp-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [http-kit "2.1.3"]]
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :main clj-wamp-example.core)
