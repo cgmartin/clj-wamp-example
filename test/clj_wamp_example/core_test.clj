9ecf8740020758d8df59f994434cf31906e498df
Christopher Martin <cgmartin@gmail.com>
2013-06-10 22:42:44 -0400

    First commit, new app project

---
+++ test/clj_wamp_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-wamp-example.core-test
+  (:require [clojure.test :refer :all]
+            [clj-wamp-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
